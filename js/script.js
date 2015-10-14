4042e6e311fb6b26d125ab0b5f7afd41e7a8d2e2
MARCHAL Diana <dmarchal@p-leleu.intra.gobelins.fr>
2015-10-14 17:42:27 +0200

    three js anims

--- js/script.js
+++ js/script.js
@@ -118,6 +118,7 @@ var average = 0.01;
 
         average = values / length;
         return average;
+        console.log(average);
     }
 
 
@@ -145,36 +146,74 @@ var average = 0.01;
     var camera = new THREE.PerspectiveCamera(45, W / H, 0.01, 10000);
 
     var planeGeometry = new THREE.PlaneGeometry(80, 80, 20, 20);
-    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true, transparent:true, opacity: 0.2});
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true, transparent:true, opacity: 0.3});
     //planeMaterial.materials[0].opacity = 0.5;
 
 
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);
     controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-    //plane.rotation.x = 2 * Math.PI;
-    //plane.materials[0].opacity = 1 + Math.sin(new Date().getTime() * .0025);
 
 
-
-
-
-    // POSITIONS //
-
-    // Tourbillon
-    plane.rotation.y = -0.5 * Math.PI;
+    // POSITION INITIALE DU PLAN //
 
     // Fontaine
-    //plane.rotation.x = -0.5 * Math.PI;
+    plane.rotation.x = -0.5 * Math.PI;
+
+    setTimeout(planeCircle, 8000);
+
+
+    function planeDna(){
+      plane.rotation.y = -0.5 * Math.PI;
+      setTimeout(planeWater, 8000);
+    }
+
+    function planeCircle() {
+      plane.rotation.x = -0.5 * Math.PI/2;
+      setTimeout(planeDna, 8000);
+    }
+
+    function planeWater() {
+      plane.rotation.x = -0.5 * Math.PI;
+      plane.rotation.y = 0;
+      console.log("change");
+      planeCircle();
+    }
+
+
+
+
+
+    //CREATION D'UNE LIGNE
+
+    var material = new THREE.LineBasicMaterial({
+      color: 0x0000ff
+    });
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(
+      new THREE.Vector3( -100, 0, 0 ),
+      new THREE.Vector3( 0, 0, 0 ),
+      new THREE.Vector3( 200, 0, 0 )
+    );
+
+    var line = new THREE.Line( geometry, material );
+
+    //line.rotation.y =   Math.PI/2;
+    // line.position.y = -200;
+   // line.rotation.x = -0.5 * Math.PI;
+    scene.add( line );
+
+
+
+
+
+
+    
+
 
     // Cercle
      //plane.rotation.x = -0.5 * Math.PI/2;
-
-
-
-
-
-
 
 
     plane.position.set(0, 0, 0);
@@ -201,20 +240,24 @@ var average = 0.01;
     // 
 
 
-    // Postprocessing glitch
-
-    // composer = new THREE.EffectComposer( renderer );
-    // composer.addPass( new THREE.RenderPass( scene, camera ) );
-
-    // glitchPass = new THREE.GlitchPass();
-    // glitchPass.renderToScreen = true;
-    // composer.addPass( glitchPass );
-
-    //
-
     document.body.appendChild(renderer.domElement);
 
     (function drawFrame(ts){
+
+
+      
+      // shape.position.y += .01;
+      // shape.position.Z += .01;
+
+      // shape.position.y += average/10000;
+      // shape.position.Z += average/10000;
+      // shape.position.x -= average/10000;
+
+      // shape.rotation.x += average/5000;
+      // shape.rotation.y += average/5000;
+
+
+
       if( !ts ) {
         window.requestAnimationFrame(drawFrame);
         return
@@ -224,6 +267,7 @@ var average = 0.01;
       window.requestAnimationFrame(drawFrame);
       var vLength = plane.geometry.vertices.length;
 
+      // 
       for (var i = 0; i < vLength; i++) {
         var v = plane.geometry.vertices[i];
         var dist = new THREE.Vector2(v.x, v.y).sub(center);
@@ -237,15 +281,38 @@ var average = 0.01;
 
         // plus on divise par un grand nombre, plus la vitesse ralentie
         var toVz = Math.sin(dist.length()/-size + (ts/500)) * magnitude;
+        console.log(toVz);
         if( toVz != NaN ) {
           v.z += ( toVz - v.z ) * .1;
         }
-
-        //group.rotation.z += 0.0001;
-        //camera.rotation.x = 1;
-        // console.log( toVz )
+      }
 
 
+
+
+
+
+      var lineLength = line.geometry.vertices.length;
+
+      for (var cpt = 0; cpt < lineLength; cpt++) {
+
+        var l = line.geometry.vertices[cpt];
+
+        var dist = new THREE.Vector2(l.x, l.y).sub(center);
+
+        var size = 5.0;
+
+        //+ on augmente magnitude, plus l'amplitude est grande, haute
+        //var magnitude = 40;
+        var magnitude = average/3;
+
+
+        // plus on divise par un grand nombre, plus la vitesse ralentie
+        var toLz = Math.sin(dist.length()/-size + (ts/500)) * magnitude;
+        if( toLz != NaN ) {
+          l.x += ( toLz - l.x ) * 10;
+          console.log(l.x);
+        }
       }
 
 
@@ -258,6 +325,10 @@ var average = 0.01;
 
 
 
+
+
+
+
     // (function(){
 
     //   var geometry = new THREE.SphereGeometry(4,2,2);
@@ -265,7 +336,7 @@ var average = 0.01;
 
     //   group = new THREE.Object3D();
 
-    //     for ( var i = 0; i < 350; i ++ ) {
+    //     for ( var i = 0; i < 10; i ++ ) {
 
     //         var mesh = new THREE.Mesh( geometry, material );
     //         // mesh.position.x = Math.random() * 2000 - 1000;
@@ -274,7 +345,7 @@ var average = 0.01;
     //         // mesh.rotation.x = Math.random() * 2 * Math.PI;
     //         // mesh.rotation.y = Math.random() * 2 * Math.PI;
 
-    //         mesh.position.x = Math.random() * 2000 - 1000;
+    //         mesh.position.x = window.innerWidth/6;
     //         mesh.position.y = Math.random() * 2000 - 1000;
     //         mesh.position.z = Math.random() * 2000 - 1000;
     //         mesh.rotation.x = Math.random() * 2 * Math.PI;
